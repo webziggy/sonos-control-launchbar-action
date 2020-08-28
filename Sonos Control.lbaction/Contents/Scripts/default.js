b88b1158ebef8871a999cbc8cb0b7a8a9d28cbf8
webziggy <alan.ogilvie@gmail.com>
2020-08-28 16:18:01 +0100

    corrected comments in javascript and readme

--- Sonos Control.lbaction/Contents/Scripts/default.js
+++ Sonos Control.lbaction/Contents/Scripts/default.js
@@ -5,7 +5,7 @@
 // This action requires:
 //   1. Hosting of a local Node-Sonos-HTTP-API - https://github.com/jishi/node-sonos-http-api
 //   2. Configuration of the serverRoot variable below to point at this.
-//   3. Configuration of the Sonos IDs variable below, this should be an array containing strings each with the 'Sonos ID' - this can be found by requesting <serverRoot>:5005/zones and looking for the 'roomName' parameters in there.
+// When action is invoked it should query the http api for 'zones' and allow find any Sonos Devices (switched on).
 
 // Configuration:
 
